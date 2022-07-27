2bf385a577728123a8025137ad24e44ce2b8c163
91898 <chandansatyendraprasad@gmail.com>
2022-07-28 01:28:10 +0530

    Initial commit

---
+++ app/src/main/java/chandan/satyendra/prasad/zero/MainActivity.kt
@@ -0,0 +1,26 @@
+package chandan.satyendra.prasad.zero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    var flag = 0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun change(view: View) {
+        var imageView = findViewById<ImageView>(R.id.imageView);
+        if (flag == 0) {
+            imageView.setImageResource(R.drawable.metallica2)
+            flag = 1
+        } else if (flag != 0) {
+            imageView.setImageResource(R.drawable.metallica1)
+            flag = 0
+        }
+    }
+}
